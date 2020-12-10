426c6348ccdb8d56308bd0cd980c1395d66574a8
Arsalan Kazmi <sonicspeed848@gmail.com>
2020-12-10 13:52:21 +0000

    Update sustop.cpp

--- sustop.cpp
+++ sustop.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+#include <string>
+#include <cstdlib>
 
 int main(int argc, char *argv[])
 {
+  string nothing;
   if (argc > 2)
   {
     cout<<"Too many arguments."<<endl;
@@ -10,6 +13,22 @@ int main(int argc, char *argv[])
   switch (argv[1])
   {
     case "-s":
-      
+      cout<<"#~ ";
+      getline();
+      cout<<"Just kidding. No root for you."<<endl;
+      break;
+    case "-v":
+      cout<<"sustop version 1"<<endl;
+      break;
+    case "-h":
+      cout<<"sustop is a joke command that does absolutely nothing of importance."<<endl<<endl<<"Feel free to check me out at https://github.com/SpeedStriker243."<<endl;
+      break;
+    case "--help":
+      cout<<"sustop is a joke command that does absolutely nothing of importance."<<endl<<endl<<"Feel free to check me out at https://github.com/SpeedStriker243."<<endl;
+      break;
+    case "-k":
+      system("exit");
+      break;
   }
+  return 0;
 }
