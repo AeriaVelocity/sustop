57e8d330be98dec0a07d341e7859dd79b3e16f1e
Arsalan Kazmi <sonicspeed848@gmail.com>
2020-12-11 13:04:19 +0000

    Update sustop.cpp

--- sustop.cpp
+++ sustop.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstdlib>
 #include <cstring>
+#include <curl>
 using namespace std;
 
 int main(int argc, char *argv[])
@@ -43,13 +44,19 @@ int main(int argc, char *argv[])
 
   if (strcmp(argv[1], "--help") == 0)
   {
-      cout<<"sustop is a joke command that does absolutely nothing of importance."<<endl<<endl<<"Feel free to check me out at https://github.com/SpeedStriker243."<<endl;
+      cout<<"sustop filename - Absolutely nothing"<<endl<<"sustop -s - Absolutely nothing, but with a message"<<endl<<"sustop -v - Prints the version number"<<endl<<"sustop -h - Prints 'help'"<<endl<<"sustop --help- Displays this message"<<endl<<"sustop -k - Terminates itself"<<endl<<"sustop -b - Attempts to run sustop in the background, but it doesn't work."<<endl;
       return 0;
   }
 
   if (strcmp(argv[1], "-k") == 0)
   {
       cout<<"This program has been terminated successfully."<<endl;
+      return 0;
+  }
+
+  if (strcmp(argv[1], "-b") == 0)
+  {
+      cout<<"Failed to open any background process."<<endl"Reason: feature not implemented"<<endl;
       return 0;
   }
 }
